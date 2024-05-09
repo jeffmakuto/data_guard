c86fbff474bc7abd9f144f71b2bf374daf3cc7c8
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-09 18:10:34 +0000

    Update __init__ and courses files

---
+++ backend/ilm/models/courses.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Course(models.Model):
+    """
+    Represents a course within the learning platform.
+    
+    Attributes:
+        title (str): Title of the course
+        description (str): Description of the course
+    """
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        """
+        Returns a string representation of the title
+        and description.
+        """
+        return f"{self.title}: {self.description}"
