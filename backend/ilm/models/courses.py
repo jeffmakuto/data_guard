cd34e9f09d0c2fbe4ccd956df9b49e2ca4f2896b
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-10 07:34:38 +0300

    Update courses.py

--- backend/ilm/models/courses.py
+++ backend/ilm/models/courses.py
@@ -12,7 +12,7 @@ class Course(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Returns a string representation of the title
         and description.
