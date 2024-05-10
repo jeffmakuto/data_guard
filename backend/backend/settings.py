110f97dd3c220ad313b5d99a93055f0b885beaca
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-10 14:59:32 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,6 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'foreign_keys': True,
+        },
     }
 }
 
