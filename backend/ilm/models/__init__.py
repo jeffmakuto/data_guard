060929db2bdcfc0b01cb9d89ca19d30a6b603722
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-11 15:26:48 +0300

    Update __init__.py

--- backend/ilm/models/__init__.py
+++ backend/ilm/models/__init__.py
@@ -1,2 +1,6 @@
 from .courses import Course
 from .modules import Module
+from .content import Content
+from .quiz import Quiz
+from .question import Question
+from .user_progress import UserProgress
