2b6dfcba08cca503cffffae514c8cb6e7d9e3e89
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-17 17:12:47 +0300

    Update init file

--- backend/ilm/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .courses import Course
-from .modules import Module
-from .content import Content
-from .quiz import Quiz
-from .questions import Question
-from .user_progress import UserProgress
-from .answers import Answer, AnswerOption
