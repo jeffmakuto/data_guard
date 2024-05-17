ec6367c36195e62a04c26d4fe6f6b924d4fc97b9
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-17 16:38:59 +0300

    class quiz, tests for the class

--- backend/ilm/models/quiz.py
+++ backend/ilm/models/quiz.py
@@ -1,15 +1,26 @@
 from django.db import models
-
+from django.core.validators import MaxLengthValidator
+from django.core.exceptions import ValidationError
 
 class Quiz(models.Model):
     """
     Represents a quiz
     """
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True, null=True, validators=[MaxLengthValidator(1000)])
 
     def __str__(self) -> str:
         """
         Return a string representation of the quiz
         """
-        return f"{self.title}: {self.description}"
+        return f"{self.title}: {self.description or 'No description'}"
+
+    def clean(self):
+        """
+        Additional validation logic.
+        """
+        if not self.title:
+            raise ValidationError('The title field cannot be blank.')
+
+        if self.description and len(self.description) > 1000:
+            raise ValidationError('The description cannot be longer than 1000 characters.')
