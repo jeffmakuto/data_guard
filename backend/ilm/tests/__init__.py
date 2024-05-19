7b2c734261c5da4b87b695c6be1ebcb4b445957f
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-19 11:20:50 +0300

    Update __init__.py - PEP8

--- backend/ilm/tests/__init__.py
+++ backend/ilm/tests/__init__.py
@@ -5,7 +5,7 @@ def suite():
     """
     Discovers all test cases within the current
     directory and subdirectories.
-    
+
     Returns a unittest.TestSuite object containing
     the discovered tests.
     """
