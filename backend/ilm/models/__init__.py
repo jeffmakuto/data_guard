c51f6ff0937786ae2ff2cc183652e8fe3434dc3e
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-21 22:53:40 +0300

    Update paths

--- backend/ilm/models/__init__.py
+++ backend/ilm/models/__init__.py
@@ -1,13 +1,11 @@
 from .courses import Course
 from .modules import Module
 from .content import Content
-from .validators import MaxFileSizeValidator
 from .serializers import CourseSerializer, ModuleSerializer, ContentSerializer
 
 
 __all__ = [
   'Course', 'Module',
-  'Content', 'MaxFileSizeValidator',
-  'CourseSerializer', 'ModuleSerializer',
-  'ContentSerializer'
+  'Content', 'CourseSerializer',
+  'ModuleSerializer', 'ContentSerializer'
 ]
