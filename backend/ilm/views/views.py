23bd87a781fa00f56c8b7ca76add489be0d7c82e
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-21 20:22:17 +0300

    Create views for the models

--- backend/ilm/views/views.py
+++ backend/ilm/views/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from models import (
+    Course,
+    Module,
+    Content,
+    CourseSerializer,
+    ModuleSerializer,
+    ContentSerializer
+)
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing courses.
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class ModuleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing modules.
+    """
+    queryset = Module.objects.all()
+    serializer_class = ModuleSerializer
+
+
+class ContentViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing contents.
+    """
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
