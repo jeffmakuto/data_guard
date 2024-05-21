d1c32fbe7fc6ed496aaa3c5f3ee140b4baceffbb
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-21 19:56:34 +0300

    Update __init__.py with the serializers classes

--- backend/ilm/models/__init__.py
+++ backend/ilm/models/__init__.py
@@ -2,5 +2,12 @@ from .courses import Course
 from .modules import Module
 from .content import Content
 from .validators import MaxFileSizeValidator
+from .serializers import CourseSerializer, ModuleSerializer, ContentSerializer
 
-__all__ = ['Course', 'Module', 'Content', 'MaxFileSizeValidator']
+
+__all__ = [
+  'Course', 'Module',
+  'Content', 'MaxFileSizeValidator',
+  'CourseSerializer', 'ModuleSerializer',
+  'ContentSerializer'
+]
