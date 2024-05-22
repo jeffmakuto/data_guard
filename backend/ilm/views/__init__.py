2174904a86f1c9a2366ef691cb51a12bc4b23967
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-22 08:53:21 +0300

    Modularize views folder

--- backend/ilm/views/__init__.py
+++ backend/ilm/views/__init__.py
@@ -1,8 +1,6 @@
-from .views import (
-    CourseViewSet,
-    ModuleViewSet,
-    ContentViewSet
-)
+from .view_courses import CourseViewSet
+from .view_modules import ModuleViewSet
+from .view_content import ContentViewSet
 
 
 __all__ = ['CourseViewSet', 'ModuleViewSet', 'ContentViewSet']
