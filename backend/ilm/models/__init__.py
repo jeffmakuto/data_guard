3255019e55cef2ee636b362662a483d48ef29c4c
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-22 08:51:28 +0300

    Relocate several files

--- backend/ilm/models/__init__.py
+++ backend/ilm/models/__init__.py
@@ -1,11 +1,6 @@
 from .courses import Course
 from .modules import Module
 from .content import Content
-from .serializers import CourseSerializer, ModuleSerializer, ContentSerializer
 
 
-__all__ = [
-  'Course', 'Module',
-  'Content', 'CourseSerializer',
-  'ModuleSerializer', 'ContentSerializer'
-]
+__all__ = ['Course', 'Module', 'Content']
