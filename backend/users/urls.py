deb2dc1285ba2f54913215b10e84874dd9a469c2
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-23 16:25:08 +0300

    Add urls file for the users

---
+++ backend/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import RegisterView, CustomTokenObtainPairView, UserDetailView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', UserDetailView.as_view(), name='user_detail'),
+]
