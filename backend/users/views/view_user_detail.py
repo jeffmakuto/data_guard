560e5f2d66a7b19f0868e50a025e41ff652d9638
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-23 16:22:39 +0300

    Update user views

---
+++ backend/users/views/view_user_detail.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from serializers import UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    """
+    Endpoint for retrieving and updating user details.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """
+        Returns the current authenticated user.
+        """
+        return self.request.user
