9f04925c0430efeb43fb05ea12a844e17530e8d4
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-24 22:27:37 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from ilm.views import CourseViewSet, ModuleViewSet, ContentViewSet
+from users.views import CustomTokenObtainPairView
 
 
 # Initialize the DefaultRouter
@@ -11,6 +12,7 @@ router.register(r'modules', ModuleViewSet)
 router.register(r'contents', ContentViewSet)
 
 urlpatterns = [
+    path('', CustomTokenObtainPairView.as_view(), name='home'),  # Set login view as the homepage
     path('admin/', admin.site.urls),  # Admin route
     path('api/v1/', include(router.urls)),  # API v1 routes for ilm app
     path('api/v1/users/', include('users.urls', namespace='users')),  # User management routes with namespace
