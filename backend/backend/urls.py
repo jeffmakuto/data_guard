47362677a8a1c31e14504dcb4edc54985b513016
jeffmakuto <hjeffmakuto@gmail.com>
2024-05-24 21:40:33 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -10,9 +10,8 @@ router.register(r'courses', CourseViewSet)
 router.register(r'modules', ModuleViewSet)
 router.register(r'contents', ContentViewSet)
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/users/', include('users.urls')),
+    path('admin/', admin.site.urls),  # Admin route
+    path('api/v1/', include(router.urls)),  # API v1 routes for ilm app
+    path('api/v1/users/', include('users.urls', namespace='users')),  # User management routes with namespace
 ]
