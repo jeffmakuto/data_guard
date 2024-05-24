556dc5af5ac3426115779f203575c22af8698d6c
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-24 23:03:45 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.http import HttpResponse
 from ilm.views import CourseViewSet, ModuleViewSet, ContentViewSet
 from users.views import CustomTokenObtainPairView
 
+
+# Define a simple view for the root URL
+def index(request):
+    return HttpResponse("Welcome to Data Guard application!")
 
 # Initialize the DefaultRouter
 router = DefaultRouter()
@@ -12,7 +17,7 @@ router.register(r'modules', ModuleViewSet)
 router.register(r'contents', ContentViewSet)
 
 urlpatterns = [
-    path('', CustomTokenObtainPairView.as_view(), name='home'),  # Set login view as the homepage
+    path('', index),  # Handle requests to the root URL
     path('admin/', admin.site.urls),  # Admin route
     path('api/v1/', include(router.urls)),  # API v1 routes for ilm app
     path('api/v1/users/', include('users.urls', namespace='users')),  # User management routes with namespace
