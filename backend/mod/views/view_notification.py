bfed678bf75f951bd690cb51412e3565ee3d5621
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-26 12:21:09 +0300

    Update view_notification.py

--- backend/mod/views/view_notification.py
+++ backend/mod/views/view_notification.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from models.notification import Notification
+from mod.models import Notification
 from serializers import NotificationSerializer
 
 
