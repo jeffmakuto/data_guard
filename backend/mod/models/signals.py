07e7adcdca2c71c1f8d35119804d6647677d0976
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-26 11:51:26 +0300

    Update signals.py

--- backend/mod/models/signals.py
+++ backend/mod/models/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from ilm.models import Course, Module, Content
-from models import Notification
+from . import Notification
 
 
 class NotificationHandler:
