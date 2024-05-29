33a0aa12162ecca4664658a2e37af0c3fa144c9c
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-29 20:32:31 +0300

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('me/', UserDetailView.as_view(), name='user_detail'),
+    path('profile/', UserDetailView.as_view(), name='user_profile'),
 ]
