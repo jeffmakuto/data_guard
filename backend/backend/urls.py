6ebe2a29c7430757ddc596909875f2817f8f6926
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-30 08:45:55 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,10 +4,13 @@ from rest_framework.routers import DefaultRouter
 from django.http import HttpResponse
 from ilm.views import CourseViewSet, ModuleViewSet, ContentViewSet
 
-
 # Define a simple view for the root URL
 def index(request):
     return HttpResponse("Welcome to Data Guard application!")
+
+# Custom error handler for 404 errors
+def custom_404(request, exception):
+    return HttpResponse("Page not found.", status=404)
 
 # Initialize the DefaultRouter
 router = DefaultRouter()
@@ -16,9 +19,12 @@ router.register(r'modules', ModuleViewSet)
 router.register(r'contents', ContentViewSet)
 
 urlpatterns = [
-    path('', index),
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/users/', include('users.urls', namespace='users')),
-    path('api/notifications/', include('mod.urls', namespace='mod'))
+    path('', index),  # Root URL
+    path('super/', admin.site.urls),  # Admin interface
+    path('api/', include(router.urls)),  # API routes
+    path('api/users/', include('users.urls', namespace='users')),  # Users app routes
+    path('api/notifications/', include('mod.urls', namespace='mod'))  # Notifications app routes
 ]
+
+# Custom error handlers
+handler404 = 'backend.urls.custom_404'
