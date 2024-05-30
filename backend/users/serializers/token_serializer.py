60fe3ded20170b4c6ec2e07231f8a5fc5cf98b2d
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-30 15:54:38 +0300

    Update token_serializer.py

--- backend/users/serializers/token_serializer.py
+++ backend/users/serializers/token_serializer.py
@@ -1,15 +1,29 @@
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from . import UserSerializer
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     """
     Custom token obtain pair serializer.
     """
+    default_error_messages = {
+        'invalid_credentials': 'Unable to log in with provided credentials.'
+    }
+
     def validate(self, attrs):
         """
         Customizes the token validation process to include user data.
         """
-        data = super().validate(attrs)
+        try:
+            data = super().validate(attrs)
+        except InvalidToken:
+            # If an invalid token is provided, raise a validation error
+            raise serializers.ValidationError(
+                self.error_messages['invalid_credentials'],
+                code='invalid_credentials'
+            )
+
+        # Add user data to the response
         data['user'] = UserSerializer(self.user).data
         return data
