f2c3f87efe2918fae818ee90659dbcb76277ab95
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-30 15:49:56 +0300

    Update register_serializer.py

--- backend/users/serializers/register_serializer.py
+++ backend/users/serializers/register_serializer.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     """
     Serializer for user registration.
     """
+    email = serializers.EmailField()
+
     class Meta:
         model = User
-        fields = '__all__'
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate_password(self, value):
+        """
+        Validates password strength.
+        """
+        try:
+            validate_password(value)
+        except ValidationError as e:
+            raise serializers.ValidationError(str(e))
+        return value
 
     def create(self, validated_data):
         """
