0aed05bc271b194ae1afe34d8beff4ee8e22a0ac
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-06-01 21:44:40 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from ilm.views import CourseViewSet, ModuleViewSet, ContentViewSet
-from users.views import RegisterView, CustomTokenObtainPairView, UserDetailView
-from rest_framework_simplejwt.views import TokenRefreshView
 
 
 # Initialize the DefaultRouter
@@ -15,9 +13,6 @@ router.register(r'contents', ContentViewSet)
 urlpatterns = [
     path('super/', admin.site.urls),  # Admin interface
     path('api/', include(router.urls)),  # API routes
-    path('api/users/register/', RegisterView.as_view(), name='register'),
-    path('api/users/login/', CustomTokenObtainPairView.as_view(), name='login'),
-    path('api/users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/users/profile/', UserDetailView.as_view(), name='user_profile'),
+    path('api/users/', include('users.urls', namespace='users')),  # Users app routes
     path('api/notifications/', include('mod.urls', namespace='mod'))  # Notifications app routes
 ]
