91e91674cfebe28dfedf29f1c016a72551a0e933
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-06-01 22:00:19 +0300

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL || 'http://localhost:8080/api',
+  baseURL: process.env.VUE_APP_BACKEND_URL || 'https://faragha.onrender.com/api'',
   headers: {
     'Content-Type': 'application/json',
   },
