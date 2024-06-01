bb1f5f6dc03ee84cacf60041a1ef0ea39ff163f0
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-06-01 21:48:18 +0300

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', RegisterView.as_view(), name='signup'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', UserDetailView.as_view(), name='user_profile'),
