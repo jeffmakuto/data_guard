9167fbe2d12c4b56a2e0e2573f94cdb3a37b17e2
jeffmakuto <hjeffmakuto@gmail.com>
2024-06-01 16:05:48 +0300

    Update vuejs code to communicate well with backend

---
+++ frontend/src/components/ModuleList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="modules-list" class="container">
+      <div v-for="module in modules" :key="module.id" class="module">
+        <strong>Module Title:</strong> {{ module.title }}<br>
+        <strong>Course:</strong> {{ module.course.title }}<br>
+        <strong>Course Description:</strong> {{ module.course.description || 'N/A' }}<br>
+        <strong>Module Description:</strong> {{ module.description || 'N/A' }}<br>
+        <hr>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      modules: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  @import '../assets/styles/module.css';
+  </style>
+  
